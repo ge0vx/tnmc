876262937667c5f326b2d94aab9a5ebec7edbfc6
giovanyperezbautista <geovapb@gmail.com>
2021-03-19 22:44:22 -0600

    parsing request Paths

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const url = require('url');
 
 //responses
 
 const server = http.createServer(function(req, res){
+
+    // Get the url and parse it
+    const parseUrl = url.pathToFileURL(req.url);
+
+    // Get the path
+    const path = parseUrl.pathname;
+    const trimPath = path.replace(/^\/+|\/+$/g,'');
+
+    // Send the response
     res.end('End\n');
+
+    // Log
+    console.log('path:', trimPath);
+    
 });
 
 //start listening
