a77743b44be5920a2ffcd0563ef310daaa00ba07
giovanyperezbautista <geovapb@gmail.com>
2021-03-27 19:01:40 -0600

    init project

---
+++ ubx/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  StatusBar,
+} from 'react-native';
+
+import HomeScreen from './src/screens/HomeScreen/index';
+
+
+const App: () => Node = () => {
+  return (
+    <>
+      <StatusBar barStyle={'dark-content'} />
+      <HomeScreen />
+    </>
+  );
+};
+
+
+export default App;
